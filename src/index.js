cc79b724d0967c1010ba1366093aa0599a93f1d6
dtnhan <dotrongnhan15102000@gmail.com>
2024-06-12 13:55:23 +0700

    fix: update hashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom'
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./store"
+import store from "./store";
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
-
 
 const options = {
   timeout: 5000,
@@ -17,18 +16,15 @@ const options = {
 };
 
 ReactDOM.render(
-  <HashRouter >
-    <Provider store={store}>
-      <AlertProvider template={AlertTemplate} {...options}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AlertProvider>
-    </Provider>
-  </HashRouter>,
-  
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </AlertProvider>
+  </Provider>,
 
+  document.getElementById("root")
+);
 
 reportWebVitals();
